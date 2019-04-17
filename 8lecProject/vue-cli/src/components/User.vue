c198d21831580dfa6fa0d6ec699c751da4dffe77
Mostafizur <mostafizur089@gmail.com>
2019-04-18 01:08:27 +0600

    upto lec111

---
+++ 8lecProject/vue-cli/src/components/User.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change My Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :myName="name" @nameWasReset="name = $event" :resetFun="resetName"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function() {
+            return {
+              name: 'Sabbir'
+            };
+        },
+        methods: {
+            changeName() {
+                this.name= 'Mostafiz';
+            },
+            resetName() {
+                this.name= 'Sabbir';
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ 8lecProject/vue-cli/src/components/UserDetail.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ myName }}</p>
+        <p>Reverse Name: {{ reverseName() }}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFun()">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        //props: ['myName'],// Get data from parent
+        props: {// props can also be a object
+            myName: {
+              type: String,
+              required: true
+            },
+            resetFun: Function// execute function of parent to the child through props
+        },
+        methods: {
+            reverseName() {
+                return this.myName.split("").reverse().join("");
+            },
+            resetName() {
+                this.myName = 'Sabbir';
+                this.$emit('nameWasReset', this.myName);// custom event
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
